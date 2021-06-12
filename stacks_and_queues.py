26ad224e3485d4e5bfbb2e2babf5708a47a09cd0
cchadha2 <chiragchadhairl@gmail.com>
2021-06-12 20:16:55 +0100

    Finishing ThreeStacks (3.1)

--- stacks_and_queues.py
+++ stacks_and_queues.py
@@ -1,48 +1,89 @@
+import unittest
+
+
 class ThreeStacks:
 
     def __init__(self, array_len):
+        if array_len < 3:
+            raise ValueError("Array must be of length 3 or greater")
+
         self.arr = [None] * array_len
-        divisor = array_len // 3
-        self.ranges = ((0, divisor), (divisor, 2*divisor), (2*divisor, array_len))
-        self.curr_idx = [elem[0] for elem in self.ranges]
+        self.divisor = array_len // 3
+        self.curr_idx = [stack * self.divisor for stack in range(3)]
 
     def peek(self, stack):
         return self.arr[self.curr_idx[stack]]
 
     def is_empty(self, stack):
         return ((self.arr[self.curr_idx[stack]] is None)
-                 and (self.curr_idx[stack] == self.ranges[stack][0]))
+                 and (self.curr_idx[stack] == stack * self.divisor))
 
     def push(self, stack, value):
-        if ((self.curr_idx[stack] == self.ranges[stack][1] - 1) and
-            (self.arr[self.curr_idx[stack]] is not None)):
+        at_end = (self.curr_idx[stack] == (stack + 1) * self.divisor - 1)
+        if at_end and self.arr[self.curr_idx[stack]] is not None:
             raise ValueError("Stack Overflow")
 
-        if not self.is_empty(stack) or self.curr_idx[stack] != self.ranges[stack][1] - 1:
+        # If we're anywhere in the middle of the stack, move the index pointer up one for the
+        # value to be inserted.
+        if not at_end and not self.is_empty(stack):
             self.curr_idx[stack] += 1
-
         self.arr[self.curr_idx[stack]] = value
 
     def pop(self, stack):
         if self.is_empty(stack):
             raise ValueError("Can't pop from empty stack")
 
-        return_val = self.arr[self.curr_idx[stack]]
-        self.arr[self.curr_idx[stack]] = None
+        return_val, self.arr[self.curr_idx[stack]] = self.arr[self.curr_idx[stack]], None
+
         if not self.is_empty(stack):
             self.curr_idx[stack] -= 1
 
         return return_val
 
 
+class TestThreeStacks(unittest.TestCase):
+    """Integration tests for ThreeStacks."""
+
+    def test_lengths(self):
+        for length in (15, 20, 3, 100, 1424124):
+            divisor = length // 3
+            stacks = ThreeStacks(array_len=length)
+
+            stacks.push(0, 15)
+            self.assertEqual(stacks.arr[0], 15)
+
+            stacks.push(1, 4)
+            self.assertEqual(stacks.arr[divisor], 4)
+
+            stacks.push(2, 9)
+            self.assertEqual(stacks.arr[2 * divisor], 9)
+
+            self.assertEqual(stacks.peek(0), 15)
+
+            self.assertEqual(stacks.pop(0), 15)
+
+            self.assertTrue(stacks.is_empty(0))
+            self.assertFalse(stacks.is_empty(1))
+
+            with self.assertRaises(ValueError):
+                stacks.pop(0)
+
+            for value in range(divisor):
+                stacks.push(0, value)
+
+            with self.assertRaises(ValueError):
+                stacks.push(0, "I'm gonna fail!")
+
+
+    def test_empty_stacks(self):
+        with self.assertRaises(ValueError):
+            ThreeStacks(0)
+
+    def test_length_less_than_three(self):
+        with self.assertRaises(ValueError):
+            ThreeStacks(2)
+
+
+
 if __name__ == "__main__":
-    length = 15
-    stacks = ThreeStacks(length)
-
-    print(stacks.push(0, 15))
-
-    print(stacks.peek(0))
-    print(stacks.is_empty(1))
-
-    print(stacks.pop(0))
-    print(stacks.is_empty(0))
+    unittest.main()
